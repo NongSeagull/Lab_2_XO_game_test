7671d19c0edafc9030d505b8fc3cdfc64b0fb3a9
Piyapong Thongman <65160230@go.buu.ac.th>
2024-07-21 14:39:55 +0700

    Initial commit

---
+++ src/main/java/com/mycompany/lab_2_xogame/Lab_2_XOgame.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lab_2_xogame;
+
+/**
+ *
+ * @author tsuna
+ */
+public class Lab_2_XOgame {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
